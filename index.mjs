4f6dd77f1e33fa9778a44cc71d7b2206238df9de
beiyehou <houge107@163.com>
2019-08-17 17:33:46 +0800

    丰富测试场景2

--- index.mjs
+++ index.mjs
@@ -1,24 +1,32 @@
 import Cpromise from './src/Cpromise.mjs';
 
 const p = new Cpromise((resolve, reject) => {
-    resolve("constructor");
-    // reject('error');
+    // resolve("constructor");
+    reject('error');
 });
 
 p.then(res => {
     console.log('1st resolve:' + res);
 }).catch(err => {
     console.log('1st reject:' + err);
-    return '1st 2nd catch return';
+    return  new Cpromise((resolve) => {
+        resolve('1st 1st catch return');
+    });
 }).then(res => {
     console.log('1st 2nd then resolve:', res);
 });
 
-
-p.catch(err => {
-    console.log('2nd reject:' + err);
+p.then(res => {
+    console.log('2nd resolve:' + res);
+}).catch(err => {
+    console.log('2nd reject:', err);
+    return '2nd 1st catch return';
 }).then(res => {
-    console.log('2nd then:', res);
-}); 
+    console.log('2nd 2nd then resolve:', res);
+});
+
+// p.catch(err => {
+//     console.log('3rd reject:' + err);
+// }); 
 
 console.log('end');
